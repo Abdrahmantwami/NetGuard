e83feee6597962e75d94e9e2182418ad486bf3db
Carlos <toxzcp@gmail.com>
2017-04-09 14:09:01 +0800

    add basic ActivityEntry UI

--- app/src/main/java/eu/faircode/netguard/ActivityEntry.java
+++ app/src/main/java/eu/faircode/netguard/ActivityEntry.java
@@ -1,10 +1,12 @@
 package eu.faircode.netguard;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
 
 /**
  * Created by Carlos on 4/8/17.
@@ -32,5 +34,17 @@ public class ActivityEntry extends AppCompatActivity {
     @Override public boolean onCreateOptionsMenu(final Menu menu) {
         if (Build.VERSION.SDK_INT < MIN_SDK) { return false; }
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.entry_firewall:
+                startActivity(new Intent(this, ActivityMain.class));
+                break;
+            case R.id.entry_virus:
+                break;
+            case R.id.entry_web:
+                break;
+        }
     }
 }
